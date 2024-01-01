8039b8a1ad46122e65b933a088ff491fd1fafc76
Jorge Herreria <33374603+jherreria@users.noreply.github.com>
2024-01-01 14:54:47 -0700

    borrame variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+varible "borrame" {
+  description = "Solo probando cosads"
+  default="DeleteMe"
+}
