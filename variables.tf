f04c51b8b9324308dd69fa769105b9707a194e93
Jorge Herreria <33374603+jherreria@users.noreply.github.com>
2024-01-01 15:16:33 -0700

    var intaance type

--- variables.tf
+++ variables.tf
@@ -2,7 +2,3 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
-variable "borrame" {
-  description = "Solo probando cosads"
-  default="t3.nano"
-}
