77695da1a4ba8b008bf66147b59d2cd238f74a9f
Jorge Herreria <33374603+jherreria@users.noreply.github.com>
2024-01-01 14:51:48 -0700

    comment added

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = variable.instance_type
-
+ # weird things
   tags = {
     Name = "HelloWorld"
   }
